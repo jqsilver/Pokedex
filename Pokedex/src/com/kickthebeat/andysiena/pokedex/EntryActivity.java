4ec4cc0e7d6e347c6d3c84aa97cacf6ae9c00184
Andy Bartholomew <abartholomew@fb.com>
2012-09-16 17:53:44 -0700

    first commit, with stubs

---
+++ Pokedex/src/com/kickthebeat/andysiena/pokedex/EntryActivity.java
@@ -0,0 +1,20 @@
+package com.kickthebeat.andysiena.pokedex;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EntryActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_entry, menu);
+        return true;
+    }
+}
